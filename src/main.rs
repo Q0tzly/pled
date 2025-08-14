3f2c638efb5f92cd96a402bd27739674fb76ffb3
Q0tzly <git@q0tzly.dev>
2025-08-14 19:38:16 +0900

    tmp: Provisionally, implemented function like cat command.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        useage();
+        exit(0);
+    };
+
+    let content = fs::read_to_string(&args[1]).expect("Error: Can't open the file.");
+    println!("{}", content);
+}
+
+fn useage() {
+    println!("Need just one arg that is file path.")
 }
